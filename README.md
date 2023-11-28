f5f12edcec8e01bcc3e2a3d64fce64e6d1ecc626
Gunho Park <devgun@kakao.com>
2023-11-28 15:18:24 +0900

    feat: save result file (csv)

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# whs-utils
+# WhiteHat School Utility
+
+**Latest Checked: November 2023**
+
 Essential utilities for mentors conducting classes in the WhiteHat School education program.
+
+<br/>
+
+## Overview
+
+This Python script, named `github_crawler.py`, serves as a utility for crawling and parsing information from the GitHub repository of WhiteHat School's Vulhub project. The script extracts data about open Pull Requests, specifically focusing on the titles of these requests.
+
+Set up a virtual environment:
+
+```bash
+python -m venv venv
+```
+
+Activate the virtual environment:
+
+-   On Windows:
+
+```bash
+.\venv\Scripts\activate
+```
+
+-   On macOS/Linux:
+
+```bash
+source venv/bin/activate
+```
+
+Install required libraries from the requirements.txt file:
+
+```bash
+pip install -r requirements.txt
+```
+
+-   Ensure that you have the required Python libraries installed in your virtual environment before running the script.
+-   The script's latest update was in November 2023.
+
+Feel free to use, modify, and enhance this utility according to your needs.
--- github_crawler.py
+++ github_crawler.py
@@ -3,29 +3,34 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-def crawl_and_parse(url):
-    response = requests.get(url)
+def crawl_and_parse(url, max_pages=6):
+    data = []
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, "html.parser")
-        pull_requests = soup.find_all(
-            "a",
-            class_="Link--primary v-align-middle no-underline h4 js-navigation-open markdown-title",
-        )
+    for page in range(1, max_pages + 1):
+        page_url = f"{url}?page={page}&q=is%3Apr+is%3Aopen"
+        response = requests.get(page_url)
 
-        data = []
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            pull_requests = soup.find_all(
+                "a",
+                class_="Link--primary v-align-middle no-underline h4 js-navigation-open markdown-title",
+            )
 
-        for pr in pull_requests:
-            print(pr, "\n")
-            data.append({"Title": pr.text.strip()})
+            for pr in pull_requests:
+                print(pr, "\n")
+                data.append({"Title": pr.text.strip()})
 
-        df = pd.DataFrame(data)
-        print(df)
+        else:
+            print(
+                f"Failed to retrieve data from page {page}. Status code: {response.status_code}"
+            )
 
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
+    df = pd.DataFrame(data)
+    df.to_csv("result.csv", index=False)
+    print(df)
 
 
 if __name__ == "__main__":
     github_url = "https://github.com/gunh0/whitehat-school-vulhub/pulls"
-    crawl_and_parse(github_url)
+    crawl_and_parse(github_url, max_pages=10)
